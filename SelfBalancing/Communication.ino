caf1f85e7a557b8b50a74be991943fd979d792f2
DinaYousseff <dinay4852@gmail.com>
2022-12-16 18:41:20 +0200

    libraries

--- SelfBalancing/Communication.ino
+++ SelfBalancing/Communication.ino
@@ -1,29 +1,4 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <WebServer.h>
-#include <ArduinoJson.h>
 
-
-const char *SSID = "your_wifi-ssid";
-const char *PWD = "your_wifi_password";
- 
-// Web server running on port 80
-WebServer server(80);
- 
-// JSON data buffer
-StaticJsonDocument<250> jsonDocument;
-char buffer[250];
- 
-// variables
-int base_angle = 0;
-int link_angle = 0;
-int grabber_angle = 0;
-float distance_M =0;
-float distance_L =0;
-float distance_R =0;
-float x_pos =0; 
-float y_pos =0; 
- 
 void connectToWiFi() {
   Serial.print("Connecting to ");
   Serial.println(SSID);
